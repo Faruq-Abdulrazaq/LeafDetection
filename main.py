50021bac7df425156a0f0f075a5b8e2a23e26445
Faruq Abdulrazaq <abdulrazaq556@gmail.com>
2023-07-25 21:47:58 +0100

    3rd

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from PIL import Image
 
 app = FastAPI()
 
-MODEL = tf.keras.models.load_model("./models/1")
+model_potato = tf.keras.models.load_model("./models/potato")
+model_pepper = tf.keras.models.load_model("./models/pepper")
 
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+CLASS_NAMES_POTATO = ["Early Blight", "Late Blight", "Healthy"]
+CLASS_NAMES_PEPPER = ["Pepper__bell___Bacterial_spot", "Pepper__bell___healthy"]
 
 
 def read_file_as_image(data) -> np.ndarray:
@@ -22,21 +24,39 @@ async def ping():
     return "Hello, I am alive"
 
 
-@app.post("/predict")
+@app.post("/predict-potato")
 async def predict(
         file: UploadFile = File(...)
 ):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
 
-    predictions = MODEL.predict(img_batch)
+    predictions = model_potato.predict(img_batch)
 
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    predicted_class = CLASS_NAMES_POTATO[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
     return {
         'class': predicted_class,
         'confidence': float(confidence)
     }
 
-if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+
+@app.post("/predict-pepper")
+async def predict(
+        file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = model_pepper.predict(img_batch)
+
+    predicted_class = CLASS_NAMES_PEPPER[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host='localhost', port=8000)
--- trainning.py
+++ trainning.py
@@ -10,14 +10,20 @@ CHANNELS = 3
 EPOCHS = 20
 
 dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    "PlantVillage",
+    "PlantPlanet",
     shuffle=True,
     image_size=(IMAGE_SIZE, IMAGE_SIZE),
     batch_size=BATCH_SIZE
 )
 class_names = dataset.class_names
 
-plt.figure(figsize=(10, 10))
+print(class_names)
+
+for image_batch, label_batch in dataset.take(1):
+    print(image_batch.shape)
+    print(label_batch.numpy())
+
+plt.figure(figsize=(20, 10))
 
 for image_batch, labels_batch in dataset.take(1):
     for i in range(12):
@@ -27,33 +33,28 @@ for image_batch, labels_batch in dataset.take(1):
         plt.axis("off")
     plt.show()
 
-training_size = 0.8
-len(dataset) * training_size
-train_dataset = dataset.take(54)
-test_dataset = dataset.skip(54)
-validation_size = 0.1
-len(dataset) * validation_size
-validation_dataset = test_dataset.skip(6)
-test_ds = test_dataset.skip(6)
-
 
 def get_dataset_partitions_tf(dataset, train_spilt=0.8, val_spilt=0.1, test_spilt=0.1, shuffle=True, shuffle_size=1000):
+    assert train_spilt + test_spilt + val_spilt == 1
     dataset_size = len(dataset)
     if shuffle:
         dataset = dataset.shuffle(shuffle_size, seed=12)
 
-    train_size = int(train_spilt * dataset_size)
-    validation_size = int(val_spilt * dataset_size)
+    train_size = int(len(dataset) * train_spilt)
+    validation_size = int(len(dataset) * val_spilt)
 
     train_dataset = dataset.take(train_size)
-
     validation_dataset = dataset.skip(train_size).take(validation_size)
-    test = dataset.skip(train_size).skip(validation_size)
+    test_dataset = dataset.skip(train_size).skip(validation_size)
 
     return train_dataset, validation_dataset, test_dataset
 
 
 train_dataset, validation_dataset, test_dataset = get_dataset_partitions_tf(dataset)
+
+print(len(train_dataset))
+print(len(validation_dataset))
+print(len(test_dataset))
 
 train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
 validation_dataset = validation_dataset.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
@@ -67,8 +68,11 @@ resize_and_rescale = tf.keras.Sequential([
 data_augmentation = tf.keras.Sequential([
     tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
     tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-
 ])
+
+train_dataset = train_dataset.map(
+    lambda x, y: (data_augmentation(x, training=True), y)
+).prefetch(buffer_size=tf.data.AUTOTUNE)
 
 input_shape = (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
 n_classes = 3
@@ -94,22 +98,23 @@ model = models.Sequential([
 ])
 
 model.build(input_shape=input_shape)
-
 model.compile(
     optimizer='adam',
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
     metrics=['accuracy']
 )
 
-history = model.fit(
-    train_dataset,
-    batch_size=BATCH_SIZE,
-    validation_data=validation_dataset,
-    verbose=1,
-    epochs=20,
-)
+if 'history' not in locals():
+    history = [model.fit(
+        train_dataset,
+        batch_size=BATCH_SIZE,
+        validation_data=validation_dataset,
+        verbose=1,
+        epochs=EPOCHS
+    )]
 
-scores = model.evaluate(test_ds)
+
+scores = model.evaluate(test_dataset)
 
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
@@ -117,7 +122,7 @@ val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-for images_batch, labels_batch in test_ds.take(1):
+for images_batch, labels_batch in test_dataset.take(1):
     first_image = images_batch[0].numpy().astype('uint8')
     first_label = labels_batch[0].numpy()
 
@@ -127,32 +132,9 @@ for images_batch, labels_batch in test_ds.take(1):
 
     batch_prediction = model.predict(images_batch)
     print("predicted label:", class_names[np.argmax(batch_prediction[0])])
+    plt.show()
 
+    plt.axis("off")
 
-def predict(model, img):
-    img_array = tf.keras.preprocessing.image.img_to_array(images[i].numpy())
-    img_array = tf.expand_dims(img_array, 0)
-
-    predictions = model.predict(img_array)
-
-    predicted_class = class_names[np.argmax(predictions[0])]
-    confidence = round(100 * (np.max(predictions[0])), 2)
-    return predicted_class, confidence
-
-
-plt.figure(figsize=(15, 15))
-for images, labels in test_ds.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-
-        predicted_class, confidence = predict(model, images[i].numpy())
-        actual_class = class_names[labels[i]]
-
-        plt.title(f"Actual: {actual_class},\n Predicted: {predicted_class}.\n Confidence: {confidence}%")
-
-        plt.axis("off")
-
-model_version = 1
-model.save(f"./models/{model_version}")
-
+model_version = ""
+model.save(f"./models/1")
