36c42a1cdd1e649c1e1a582d73db0652d26bca7e
Faruq Abdulrazaq <abdulrazaq556@gmail.com>
2023-07-31 23:51:02 +0100

    Cors added

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import tensorflow as tf
 import numpy as np
 import uvicorn
 from io import BytesIO
 from PIL import Image
 
+
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://leafdectectionfrontend.web.app",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model_potato = tf.keras.models.load_model("./models/potato")
 model_pepper = tf.keras.models.load_model("./models/pepper")
@@ -59,4 +74,4 @@ async def predict(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=3000)
+    uvicorn.run(app, host='localhost', port=8000)
